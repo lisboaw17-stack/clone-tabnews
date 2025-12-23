b38fd76828b91a9c69ee7cf40fe3038dad3de764
WELLINGTON DE LISBOA PEREIRA <lisboaw17@gmail.com>
2025-12-23 14:19:15 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <h1>Nicolly meu amor, eu te amo. Se você me ama, de uma risadinha! </h1>
+function Home() {
+  return (
+    <h1>Nicolly meu amor, eu te amo. Se você me ama, de uma risadinha! </h1>
+  );
 }
 
 export default Home;
